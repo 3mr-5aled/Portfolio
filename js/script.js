bd40f21d698b624a079581b7a142a51b07849cac
Amr Khaled <moroamr2005@gmail.com>
2021-06-02 11:51:46 +0200

    new navigation feature

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@
 mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -20,3 +22,17 @@ function topFunction() {
 function myFunction(x) {
   x.classList.toggle("change");
 }
+
+function showPage(page) {
+  document.querySelectorAll("article > div").forEach((div) => {
+    div.style.display = "none";
+  });
+  document.querySelector(`#${page}`).style.display = "grid";
+}
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("#pages").forEach((pages) => {
+    pages.onclick = function () {
+      showPage(this.dataset.page);
+    };
+  });
+});
