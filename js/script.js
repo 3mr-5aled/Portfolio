f8d7709c209506884ff841e8034e0a02144f7d2a
Amr Khaled <moroamr2005@gmail.com>
2021-07-05 10:48:24 +0200

    new smoothy scroll to top

--- js/script.js
+++ js/script.js
@@ -1,28 +1,14 @@
-//Get the button:
-mybutton = document.getElementById("myBtn");
+let up = document.querySelector(".up");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
-  scrollFunction();
+  this.scrollY >= 200 ? up.classList.add("show") : up.classList.remove("show");
 };
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-function myFunction(x) {
-  x.classList.toggle("change");
-}
-
+up.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
 function showPage(page) {
   document.querySelectorAll("article > div").forEach((div) => {
     div.style.display = "none";
