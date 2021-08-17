c0348dc7a4ae6f4dce324689ab8f3f80ccbbd482
Amr Khaled <moroamr2005@gmail.com>
2021-08-17 11:29:42 +0200

    Full Page tabs

--- js/script.js
+++ js/script.js
@@ -9,16 +9,33 @@ up.onclick = function () {
     behavior: "smooth",
   });
 };
-function showPage(page) {
-  document.querySelectorAll("article > div").forEach((div) => {
-    div.style.display = "none";
-  });
-  document.querySelector(`#${page}`).style.display = "grid";
+// function showPage(page) {
+//   document.querySelectorAll("article > div").forEach((div) => {
+//     div.style.display = "none";
+//   });
+//   document.querySelector(`#${page}`).style.display = "grid";
+// }
+// document.addEventListener("DOMContentLoaded", function () {
+//   document.querySelectorAll("#pages").forEach((pages) => {
+//     pages.onclick = function () {
+//       showPage(this.dataset.page);
+//     };
+//   });
+// });
+// Full Page Tabs
+function openPage(pageName, elmnt, color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(pageName).style.display = "block";
+  elmnt.style.backgroundColor = color;
 }
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll("#pages").forEach((pages) => {
-    pages.onclick = function () {
-      showPage(this.dataset.page);
-    };
-  });
-});
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
