981f0691325918a3de495f6e53d496b4d44611de
3mr-5aled <moroamr2005@gmail.com>
2023-02-04 16:44:47 +0200

    check cms

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
-// const pug = require("pug")
 module.exports = function (eleventyConfig) {
   eleventyConfig.setPugOptions({ pretty: true })
+  eleventyConfig.addPassthroughCopy("src/admin")
   return {
     dir: {
       input: "src",
